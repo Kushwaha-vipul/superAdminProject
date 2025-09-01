830125210e0fbadd966f8dd9d2d8cb35d4d3eeb0
Vipul Kumar Kushwaha <100346094+Kushwaha-vipul@users.noreply.github.com>
2025-09-01 06:18:02 +0530

    Update seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -5,7 +5,7 @@ const sequelize = require('./config');
 async function seed() {
     await sequelize.sync({ force: true });
 
-    // 1. Roles create karo dynamically
+   
     const roleNames = ['superadmin', 'user', 'manager'];
     const roles = [];
     for (const name of roleNames) {
@@ -13,16 +13,16 @@ async function seed() {
         roles.push(role);
     }
 
-    // 2. Super Admin create karo aur role assign karo
+
     const hashedPassword = await bcrypt.hash('Test1234!', 10);
     const superAdmin = await User.create({
         name: 'Super Admin',
         email: 'superadmin@example.com',
         hashedPassword,
-        roles: [] // initially empty
+        roles: []
     });
 
-    // 3. Assign superadmin role from Roles table
+   
     const superadminRole = roles.find(r => r.name === 'superadmin');
     superAdmin.roles = [superadminRole.name];
     await superAdmin.save();
